19c983e90088d298802a81c7d125d430608cf091
Joe Sun <joesun@Joes-MacBook-Air.local>
2022-09-01 11:31:57 +0200

    Project relocation

---
+++ src/components/Result.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import QuestionResult from './QuestionResult';
+import './Result.css';
+
+const Result = ({ userId, quizId }) => {
+  const [userResult, setUserResult] = useState(null);
+  const [isOpen, setIsOpen] = useState('');
+
+  const handleIsOpen = questionText => {
+    setIsOpen(questionText);
+  };
+
+  useEffect(() => {
+    fetch(`https://getit-app.azurewebsites.net/api/Users/${userId}/quizzes/${quizId}`)
+      .then(response => response.json())
+      .then(json => setUserResult(json));
+  }, []);
+
+  return (
+    <>
+      {(userResult) && (
+        <div className="userResult__container">
+          <h1>{userResult.quizTitle}</h1>
+          <h2>{userResult.username}</h2>
+          <div className="userResult__scoreContainer">
+            <h3>
+              Your score:
+              <span className="userResult__score">{`${userResult.score}%`}</span>
+            </h3>
+          </div>
+          {userResult.questions.map(question => (
+            <QuestionResult
+              question={question}
+              isOpen={isOpen}
+              handleIsOpen={handleIsOpen} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Result;
